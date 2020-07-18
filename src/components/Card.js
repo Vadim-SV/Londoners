4f398cc2091a8efcac0717bc0880084f00ac168f
Your NameVadim-SV <syubaew@gmail.com>
2020-07-18 16:24:25 +0100

    implemented notrefferer

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({name, email, id}) => {
 	return (
 
 		 <div className = " ma2-ns tc bg-white dib br3 ma2 pa3 grow bw3 shadow-5 " > 
-     <a className = "card-link no-underline-ns" href = "https://www.zoosk.com/personals/search/0/about/" target="_blank">
+     <a className = "card-link no-underline-ns" href = "https://www.zoosk.com/personals/search/0/about/" rel="noreferrer" target="_blank">
           <img alt='robots'  src={`https://joeschmoe.io/api/v1/${id}?size=200x200`} href = "https://www.zoosk.com/personals/search/0/about/" />
           <div>
             <h2> {name} </h2>
